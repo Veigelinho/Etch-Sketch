91ec2fecfd1319048b32fab6ad4607c2f49d9a6b
Marian Veigel <marian.veigel@googlemail.com>
2023-03-26 18:22:36 +0200

    Changed layout, fixed bad coding, all works now except for the gridsize input slider

--- script.js
+++ script.js
@@ -15,13 +15,16 @@ rubberButton.innerText = "RUBBER"
 
 const drawButton = document.createElement("button")
 drawButton.classList.add("draw")
-drawButton.innerText = "DRAW"
+drawButton.innerText = "SINGLE"
 
 const rainbowButton = document.createElement('button')
 rainbowButton.innerText = "RAINBOW"
 
 const shadesButton = document.createElement('button')
 shadesButton.innerText = '50 SHADES'
+
+const sizePicker = document.getElementById('gridsizepicker')
+
 
 
 for (let i = 1; i < 257; i++) {
@@ -33,15 +36,17 @@ for (let i = 1; i < 257; i++) {
 
 }
 
-leftBar.append(colorPicker, drawButton, rainbowButton)
-rightBar.append(rubberButton, shadesButton)
+leftBar.append(drawButton, rainbowButton, shadesButton, rubberButton)
+rightBar.append(colorPicker)
 
 
 const DEFAULT_COLOR = '#333333'; 
 const DEFAULT_MODE = 'color';
+const DEFAULT_SIZE = 16;
 
 var currentColor = DEFAULT_COLOR;
 var currentMode = DEFAULT_MODE;
+var currentSize = DEFAULT_SIZE;
 
 function setCurrentColor(newColor) {
     currentColor = newColor;
@@ -51,20 +56,29 @@ function setCurrentMode(newMode) {
     currentMode = newMode;
 }
 
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
 const body = document.body
 
 
 
 
 
-colorPicker.oninput = (e) => setCurrentColor(e.target.value)
+colorPicker.oninput = (e) => setCurrentColor(e.target.value), setCurrentMode('color')
 drawButton.onclick = () => setCurrentMode('color')
-rubberButton.onclick = () => setCurrentMode('eraser')
+rubberButton.onclick = () => setCurrentMode('rubber')
 rainbowButton.onclick = () => setCurrentMode('rainbow')
 shadesButton.onclick = () => setCurrentMode('shades')
+sizePicker.oninput = (e) => createGrid(e)
 
 
 
+function createGrid (size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+}
 
 
 function changeColor(e) {
@@ -73,7 +87,7 @@ function changeColor(e) {
         console.log(e.target.style.backroundColor)
         }
 
-    else if (currentMode == 'eraser') {
+    else if (currentMode == 'rubber') {
         e.target.style.backgroundColor = 'white'
     }
 
@@ -87,57 +101,10 @@ function changeColor(e) {
 
     else if (currentMode == 'shades') {
         const opacity = Math.floor(Math.random() * 256).toString(16);
-        const currentColorString = currentColor.slice(1)
-        console.log(currentColorString)
-       e.target.style.backgroundColor = `${currentColor}${opacity}`
+        e.target.style.backgroundColor = `${currentColor}${opacity}`
 
     }
 }
 
 
-
-// containerDiv.onmouseover = (e) => classAddRed(e)
-
-// function rubber (e) {
-//     if (e.target.matches("div.container > *")) {
-//         e.target.removeEventListener('onmouseover', classAddRed)
-//         e.target.classList.remove("hoverred")
-//         e.target.classList.add("hoverwhite")
-        
-//     }
-// }
-
-// function classAddRed(e) {
-//     if (e.target.matches("div.container > *")) {
-//         e.target.classList.add("hoverred")
-//         if (e.target.classList.contains("hoverwhite")) {
-//             e.target.classList.remove("hoverwhite")
-//         }
-        
-        
-//     }
-// }
-
-// rubberButton.onclick = () => {
-//     grid.removeEventListener('mouseover', classAddRed)
-//     grid.addEventListener('mouseover', rubber)
-// }
-
-// drawButton.onclick = () => {
-//     grid.classList.remove('hoverwhite')
-//     grid.removeEventListener('mouseover', rubber)
-//     grid.addEventListener('mouseover', classAddRed)
-    
-// }
-
-// function removeRedEvent(e) {
-//     if (e.target.matches("button")) {
-//         console.log(e)
-        // let gridelms = document.querySelectorAll("div.container > *")
-        // for (elements of gridelms) {
-        //     gridelms.classList.remove("hoverred")
-        // }
-        // containerDiv.removeEventListener("mouseover", eventListenerAddRed)
-//     }
-// }
 
